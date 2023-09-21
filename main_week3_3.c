2e7b712a866939488ef4723c1f5586ab8a3acf7c
cyj4972 <144991384+cyj4972@users.noreply.github.com>
2023-09-21 21:40:34 +0900

    Create main_week3_3.c

---
+++ main_week3_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float a, b;
+	
+	printf("분자를 입력하세요:");
+	scanf("%f", &a);
+	
+	printf("분모를 입력하세요:");
+	scanf("%f", &b);
+	
+	printf("나누기의 결과는 %f 입니다", (float)a/b);
+	
+	system("PAUSE");
+	return 0;
+}
